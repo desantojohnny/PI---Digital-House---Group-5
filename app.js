0ec51876c006bcf081653980c080a100644e47d4
desantojohnny <johnny.santo@gmail.com>
2022-06-10 11:28:27 -0300

    organizacao de pasta

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const produtos = require('./routes/produtos');
+const produtos = require('./src/routes/produtos');
 let app = express();
 
 app.get('/', (req, res)=> res.send('Olá Mundos!'));
 app.get('/contatos', (req, res)=>res.send('Página Contatos'));
-
+app.get('/produtos/:id', (req, res)=>{
+    req.params.id;
+    console.log('Eu tenho o produto com o o id:', id)
+})
 app.use('/produtos', produtos);
 
 //criando servidor
+app.listen(3000, ()=>console.log('Servidor rodando na porta 3000'))
+
+
+
+
+
+
+
+
+
+
 /* const app = express();
 const path = require('path');
 
