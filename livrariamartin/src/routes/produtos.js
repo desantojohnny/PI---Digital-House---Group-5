fcafa629cbecddf9604c7bebd6d30aa282f8f564
desantojohnny <johnny.santo@gmail.com>
2022-06-10 11:58:53 -0300

    organizacao pasta conforme express ejs

--- livrariamartin/src/routes/produtos.js
+++ livrariamartin/src/routes/produtos.js
@@ -7,3 +7,5 @@ router.get('/', produtosController.index);
 router.get('/', (req, res)=>{
     res.send('Index de produtos');
 });  esse era o modo antigo de fazer agora com o MVC desenvolvemos como acima*/
+
+/* ARQUIVO DE AULA NÃO NECESSÁRIO PARA O PROJETO*/
