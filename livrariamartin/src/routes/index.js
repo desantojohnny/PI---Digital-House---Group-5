0120659dc09f4509bfb859ac54f686f90d285706
desantojohnny <johnny.santo@gmail.com>
2022-06-15 08:48:14 -0300

    contato ejs

--- livrariamartin/src/routes/index.js
+++ livrariamartin/src/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var contatoController = require("../controllers/contatoController")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/contato", contatoController.index);
 
 module.exports = router;
