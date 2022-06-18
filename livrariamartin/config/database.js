4d6d05cac87ab5188a1689b0ce0d92c655d9a285
desantojohnny <johnny.santo@gmail.com>
2022-06-18 15:05:23 -0300

    sequelize

---
+++ livrariamartin/config/database.js
@@ -0,0 +1,20 @@
+const config ={
+    username: 'root',
+    password: '',
+    database: 'livraria',
+    host: 'localhost',
+    dialect: 'mysql'
+}
+
+module.exports = config
+
+
+// const path = require('path')
+
+// module.exports = {
+//     config: path.resolve('./database/config', 'config.js'),
+//     'models-path': path.resolve('./database/models'),
+//     'seeders-path': path.resolve('./database/seeders'),
+//     'migrations-path': path.resolve('./database/migrations'),
+
+// }
