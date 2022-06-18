c693a5c1cc1d3cd568ec517a1d04cb45004a21b2
desantojohnny <johnny.santo@gmail.com>
2022-06-18 15:39:39 -0300

    config sequelize

---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataType) =>{
+    const Usuario = sequelize.define('Usuario', {
+        id_usuario:{
+            type:DataType.INTERGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome:{
+            type: DataType.STRING,
+        },
+            email:{
+                type:DataType.STRING,
+                allowNull:false
+            },
+            senha:DataType.STRING
+    }, { //padrao do sequelize, deixando no singular, assim ele deixara conforme o nome que escolhermos (MANTER NO SINGULAR)
+        tableName: 'usuario',
+        timestamps: false
+    })
+
+    return Usuario
+
+}
